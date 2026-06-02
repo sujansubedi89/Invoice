0b0201e09bae3d95d552bdaf19db0a533f4da7a9
sujan subedi <sujansubedinp@gmail.com>
2026-06-02 16:05:38 +0545

    updated

--- invoice_system/settings.py
+++ invoice_system/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-invoice-system-secret-key-change-in-production'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
