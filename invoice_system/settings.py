8023af8f798c881f9dce3f1e3c5ae0627eb368b8
sujan subedi <sujansubedinp@gmail.com>
2026-06-02 22:01:18 +0545

    invoiceupdated

--- invoice_system/settings.py
+++ invoice_system/settings.py
@@ -55,19 +55,19 @@ WSGI_APPLICATION = 'invoice_system.wsgi.application'
 
 
 # Update DATABASES for production (e.g., PostgreSQL)
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'invoice_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'pass',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
+#     'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invoice_db',
+        'USER': 'postgres',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
 ]
